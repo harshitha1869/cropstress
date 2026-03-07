251a1d4b1d7ed77d0bbf2d59e4bdf37ddb028c7a
harshitha1869 <23b01a12h7@svecw.edu.in>
2026-03-07 15:51:34 +0530

    fixed api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 export async function predictCropDamage(formData) {
-  const response = await fetch("http://localhost:4000/predict ", {
+  const response = await fetch("https://cropstress.onrender.com/predict", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
